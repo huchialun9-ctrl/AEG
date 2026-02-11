e5672406475425bba1fff0a7b01ad27aff0d70ff
簡毓芳 <jianyufang@jianyufangs-MacBook-Air.local>
2026-02-11 09:23:59 +0800

    Fix DEX icon and remove Discord social link

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,16 @@ import "@nomicfoundation/hardhat-ethers";
 import "dotenv/config";
 
 export default {
-    solidity: "0.8.20",
+    solidity: {
+        version: "0.8.24",
+        settings: {
+            evmVersion: "cancun",
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
+    },
     networks: {
         "base-sepolia": {
             url: "https://sepolia.base.org",
